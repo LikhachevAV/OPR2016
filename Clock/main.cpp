0fc9daca94873c6058e3305d7dd511959721774a
YolloPukki123 <temik_l@mail.ru>
2016-12-23 21:26:13 +0300

    Добавил часовую стрелку

--- Clock/main.cpp
+++ Clock/main.cpp
@@ -15,9 +15,10 @@
 const int screenWidth = 800;
 const int screenHeight = 600;
 const auto PI = M_PI;
-const int clockCircleSize = 250;
-const int clockCircleDotsCount = 100;
-const int clockCircleThickness = 2;
+const float clockCircleRadius = 250;
+const float  centerCircleRadios = 10;
+const size_t circlesDotsCount = 100;
+const float clockCircleThickness = 2;
 const int characterSize = 20;
 const std::string fontPath = "resources/arial.ttf";
 const int hoursCount = 12;
@@ -25,15 +26,20 @@ const int minutesCount = 60;
 const int maxAngle = 360;
 const int hoursRotationDegree = maxAngle / hoursCount;
 const int minutesRotationDegree = maxAngle / minutesCount;
+const float hoursHandWidth = 5;
+const float hoursHandHeight = 180;
+const sf::Color hoursHandColor = sf::Color::Black;
 
 struct  Clock
 {
 	sf::CircleShape clockCircle;
+	sf::CircleShape centerCircle;
 	sf::Vector2f center;
 	sf::CircleShape hoursMarker[hoursCount];
 	sf::RectangleShape minutsMarker[minutesCount];
 	sf::Font font;
 	sf::Text hoursDigits[hoursCount];
+	sf::RectangleShape hourHand;
 	//sf::Music clockTick;
 	Clock(sf::RenderWindow & window)
 	{
@@ -41,13 +47,46 @@ struct  Clock
 	}
 };
 
+sf::RectangleShape GetHand(float width, float height, const sf::Vector2f & center, const sf::Color & color)
+{
+	sf::RectangleShape hand(sf::Vector2f(width, height));
+	hand.setFillColor(color);
+	hand.setOrigin(hand.getGlobalBounds().width / 2, hand.getGlobalBounds().height - 25);
+	hand.setPosition(center);
+	return hand;
+}
+
+void InitHourHand(Clock & clock)
+{
+	clock.hourHand = GetHand(hoursHandWidth, hoursHandHeight, clock.center, hoursHandColor);
+}
+
+void InitMinutesHand(Clock & clock)
+{
+	//TODO
+}
+
+void InitSecondsHand(Clock & clock)
+{
+	//TODO
+}
+
 void InitClockCircle(Clock & clock)
 {
-	clock.clockCircle = sf::CircleShape(clockCircleSize, clockCircleDotsCount);
+	clock.clockCircle = sf::CircleShape(clockCircleRadius, circlesDotsCount);
 	clock.clockCircle.setOutlineThickness(clockCircleThickness);
 	clock.clockCircle.setOutlineColor(sf::Color::Black);
-	clock.clockCircle.setOrigin(clock.clockCircle.getGlobalBounds().width / 2, clock.clockCircle.getGlobalBounds().height / 2);
+	clock.clockCircle.setOrigin(clock.clockCircle.getGlobalBounds().width / 2,
+		clock.clockCircle.getGlobalBounds().height / 2);
 	clock.clockCircle.setPosition(clock.center.x + clockCircleThickness, clock.center.y + clockCircleThickness);
+}
+
+void InitCenterCircle(Clock & clock)
+{
+	clock.centerCircle = sf::CircleShape(centerCircleRadios, circlesDotsCount);
+	clock.centerCircle.setFillColor(sf::Color::Red);
+	clock.centerCircle.setOrigin(clock.centerCircle.getGlobalBounds().width / 2, clock.centerCircle.getGlobalBounds().height / 2);
+	clock.centerCircle.setPosition(clock.center);
 }
 
 void InitHoursMarkers(Clock & clock)
@@ -56,8 +95,8 @@ void InitHoursMarkers(Clock & clock)
 	float angle = 0.0;
 	for (int i = 0; i < 12; ++i)
 	{
-		currentPosition.x = (clockCircleSize - 10) * cos(angle);
-		currentPosition.y = (clockCircleSize - 10) * sin(angle);
+		currentPosition.x = (clockCircleRadius - 10) * cos(angle);
+		currentPosition.y = (clockCircleRadius - 10) * sin(angle);
 
 		clock.hoursMarker[i] = sf::CircleShape(3);
 		clock.hoursMarker[i].setFillColor(sf::Color::Black);
@@ -86,7 +125,7 @@ void InitHoursDigits(Clock & clock)
 		clock.hoursDigits[i].setFont(clock.font);
 		clock.hoursDigits[i].setFillColor(sf::Color::Black);
 		clock.hoursDigits[i].setString(std::to_string(i + 1));
-		auto differenceBetweenhoursDigitsAndDots = (float)(clockCircleSize - 2 * characterSize);
+		auto differenceBetweenhoursDigitsAndDots = (float)(clockCircleRadius - 2 * characterSize);
 		sf::Vector2f pos = differenceBetweenhoursDigitsAndDots * sf::Vector2f(cos(angle), sin(angle));
 		sf::Vector2f absolutePosition(pos + clock.center);
 		sf::FloatRect rect = clock.hoursDigits[i].getGlobalBounds();
@@ -103,8 +142,8 @@ void InitMinutesMarkers(Clock & clock)
 	float angle = 0.0;
 	for (int i = 0; i < minutesCount; i++)
 	{
-		currentPosition.x = (clockCircleSize - 10) * cos(angle);
-		currentPosition.y = (clockCircleSize - 10) * sin(angle);
+		currentPosition.x = (clockCircleRadius - 10) * cos(angle);
+		currentPosition.y = (clockCircleRadius - 10) * sin(angle);
 
 		if (i % 5 != 0)
 		{
@@ -127,6 +166,10 @@ void InitClock(Clock & clock)
 	InitHoursDigits(clock);
 	InitHoursMarkers(clock);
 	InitMinutesMarkers(clock);
+	InitCenterCircle(clock);
+	InitHourHand(clock);
+	InitMinutesHand(clock);
+	InitSecondsHand(clock);
 }
 
 void HandleEvents(sf::Window & window)
@@ -141,6 +184,22 @@ void HandleEvents(sf::Window & window)
 
 void DrawClock(sf::RenderWindow & window, Clock & clock)
 {
+	auto drawHourHand = [&]() {
+		window.draw(clock.hourHand);
+	};
+
+	auto drawMinutesHand = [&]() {
+		//TODO
+	};
+
+	auto drawSecondsHand = [&]() {
+		//TODO
+	};
+
+	auto drawCenterCircle = [&]() {
+		window.draw(clock.centerCircle);
+	};
+
 	auto drawClockCircle = [&]() {
 		window.draw(clock.clockCircle);
 	};
@@ -173,6 +232,10 @@ void DrawClock(sf::RenderWindow & window, Clock & clock)
 	drawHoursDigits();
 	drawHoursMarkers();
 	drawMinutesMarkers();
+	drawHourHand();
+	drawMinutesHand();
+	drawSecondsHand();
+	drawCenterCircle();
 	window.display();
 }
 
